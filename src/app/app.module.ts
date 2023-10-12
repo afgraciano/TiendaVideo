6aa2916e24b6b4b0636a0e452acf4736e86a16f9
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-10-12 07:51:11 -0500

    1commitPracticaAngularArquitecturaSoftware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { EmpresasModule } from './componentes/empresas/empresas.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    EmpresasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/empresas/empresas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmpresasComponent } from './paginas/empresas/empresas.component';
+
+
+
+@NgModule({
+  declarations: [EmpresasComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmpresasModule { }
